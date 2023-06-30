96366e4a8a7be74a245ec13388d2234c6984831e
PinkPaca <125029028+PinkPaca@users.noreply.github.com>
2023-06-30 10:11:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   console.log(req.body.cityName);
   const query = req.body.cityName;
-  const apiKey = "4f80dad81f0c52900ac730fcc1859595";
+  const apiKey = "";
   const unit = "metric";
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey+ "&units=" + unit;
   
